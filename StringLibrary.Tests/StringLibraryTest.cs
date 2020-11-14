190819995718b1829c4b201c1a24cae099b4d797
Brian Jablonsky <bjablonsky@users.noreply.github.com>
2020-11-14 11:38:23 -0500

    Week 8 Project Unit Testing Example

---
+++ StringLibrary.Tests/StringLibraryTest.cs
@@ -0,0 +1,92 @@
+using System;
+using Xunit;
+using StringLibrary;
+
+namespace StringLibrary.Tests
+{
+    public class StringLibraryTest
+    {
+        [Fact]
+        public void TestStartsWithUpperWhenNullOrEmpty()
+        {
+            // Tests that we expect to return false.
+
+            // Arrange
+            string[] words = { String.Empty, null };
+            foreach (var word in words)
+            {
+                // Act
+                bool result = StringLibrary.StartsWithUpper(word);
+
+                // Asset
+                Assert.False(result);
+            }
+        }
+
+        [Fact]
+        public void TestStartsWithLowerWhenNullOrEmpty()
+        {
+            // Tests that we expect to return false.
+
+            // Arrange
+            string[] words = { String.Empty, null };
+            foreach (var word in words)
+            {
+                // Act
+                bool result = StringLibrary.StartsWithLower(word);
+
+                // Assert
+                Assert.False(result);
+            }
+        }
+
+        [Fact]
+        public void TestStartsWithLower()
+        {
+            // Tests that we expect to return false.
+            // Arrange
+            string[] words = { "alphabet", "zebra", "abc"};
+            foreach (var word in words)
+            {
+                // Act
+                bool result = word.StartsWithLower();
+
+                // Assert
+                Assert.True(result);
+            }
+        }
+
+        [Fact]
+        public void TestDoesNotStartWithLower()
+        {
+            // Tests that we expect to return true.
+            // Arrange
+            string[] words = { "Alphabet", "Zebra", "ABC", "1234", ".", ";", " "};
+            foreach (var word in words)
+            {
+                // Act
+                bool result = word.StartsWithLower();
+
+                // Assert
+                Assert.False(result);
+            }
+        }
+
+        [Fact]
+        public void TestHasEmbeddedSpaces()
+        {
+            // Tests that we expect to return true.
+            // Arrange
+            string[] phrases = { "one car" };
+
+            foreach (var phrase in phrases)
+            {
+                // Act
+                bool result = phrase.HasEmbeddedSpaces();
+
+                // Assert
+                Assert.True(result);
+            }
+        }
+    }
+}
